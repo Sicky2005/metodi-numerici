c24f7945c066920c9b249cf14335003e748e87dd
Sicky2000 <giuseppecolia153@gmail.com>
2025-12-16 12:27:09 +0100

    Added Trapezoidal Rule.

---
+++ integrals/trapezoidal.py
@@ -0,0 +1,26 @@
+"""
+    Autore: Sicky2005
+    Corso: Metodi Numerici per l'Ingegneria
+    Descrizione: Programma per il Metodo dei Trapezi
+"""
+
+def metodo_trapezi(func, a, b, n):
+    """
+    func: La funzione da integrare
+    a: Inizio dell'intervallo
+    b: Fine dell'intervallo
+    n: Numero di intervalli
+    """
+    # Calcolo del passo (h)
+    h = (b - a) / n
+
+    # Valutazione estremi (f(a) + f(b))
+    somma = func(a) + func(b)
+
+    # Sommatoria dei punti interni moltiplicati per 2
+    for i in range(1, n):
+        x = a + i * h
+        somma += 2 * func(x)
+
+    # Calcolo finale
+    return (h / 2) * somma
